0926e39cfdc8595f9cd376275a761defb491f5a6
KangWoosung <53413020+KangWoosung@users.noreply.github.com>
2024-03-19 17:08:37 +0900

    Init Commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+// import { useState } from "react";
+
+import StarRater from "./components/StarRater";
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>StarRater-CSR</h1>
+      <StarRater />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/StarRater.tsx
@@ -0,0 +1,16 @@
+/*  2024-03-19 17:03:09
+
+
+*/
+
+import React from "react";
+
+const StarRater = () => {
+  return (
+    <>
+      <h3>StarRater</h3>
+    </>
+  );
+};
+
+export default StarRater;
